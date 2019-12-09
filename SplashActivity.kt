f15f27a6fc0cf6dede4a9731217c02d7d8b5ccec
sonhyenoh <ok7624583@naver.com>
2019-12-09 23:44:56 +0900

    Add files via upload
    구현은 거의 다끝났고 이제 디자인을 손봤습니다. splash도 썼고 design을 고민하던 끝에 배달통에서 쓰던 색을 가져와서 메인으로 잡고 그 뒤에 배경을 맞춘후 원색으로 깔끔하게 디자인했습니다. 생각보다 디자인에서 시간이 많이 쏟았지만 나름 뿌듯했고 디자인은 앞으로 이쪽으로 가면 될듯합니다.

--- SplashActivity.kt
+++ SplashActivity.kt
@@ -1,17 +1,16 @@
-package com.example.qreservationapp
+package com.example.reservationapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity : AppCompatActivity() {
-
+class SplashActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, MainActivity::class.java)            // 실제 사용할 메인 액티비티
+        
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 }
-
